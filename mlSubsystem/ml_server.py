b0d0d7045afd67d5291231c25dd78aa22e6b649c
TownsendSP <tgsoutha@syr.edu>
2024-10-22 01:26:26 -0400

    ML communication with sockets

---
+++ mlSubsystem/ml_server.py
@@ -0,0 +1,87 @@
+import socket
+import pickle
+import sys
+from PIL import Image
+from transformers import AutoProcessor, Blip2ForConditionalGeneration
+import torch
+import configreader
+
+host = None  # localhost
+port = None  # arbitrary non-privileged port
+model_directory = None
+processor = None
+model = None
+device = None
+
+
+def process_image(image: Image, prompt):
+    global processor, model, device
+    image = image.convert('RGB')
+    inputs = processor(image, text=prompt, return_tensors="pt").to(device, torch.float16)
+    generated_ids = model.generate(**inputs)
+    generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
+    return generated_text
+
+
+def wait_for_messages():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.bind((host, port))
+            s.listen()
+            print("Receiver is waiting for messages...")
+            while True:
+                conn, addr = s.accept()
+                with conn:
+                    data = conn.recv(1024)
+                    if data:
+                        try:
+                            message = pickle.loads(data)
+                            # the message should be a tuple with image object and prompt
+                            prompt = message[1]
+                            image = message[0]
+                            # get metadata from the image to print for debug
+                            img_metadata = f"Image: {image.format}, {image.size}, {image.mode}"
+                            print(f"Received:\n\tImage: {img_metadata}\n\tPrompt: {prompt}")
+                            generated_text = process_image(image, prompt)
+                            conn.sendall(pickle.dumps({"status": 0, "message": generated_text}))
+                        except pickle.UnpicklingError:
+                            print("Error: Unable to unpickle the received data")
+                            conn.sendall(pickle.dumps({"status": 1, "message": "Unpickling Error"}))
+                        except Exception as e:
+                            print(f"Error: {str(e)}")
+                            conn.sendall(pickle.dumps({"status": 2, "message": f"General Error: {str(e)}"}))
+        except OSError as e:
+            print(f"Error: {str(e)}")
+            return 1
+    return 0
+
+
+def main():
+    #region Read Configuration
+    global host, port, model_directory, processor, model, device
+    try:
+        mlConfig = configreader.read_config('ml.cfg')
+        host = mlConfig.ip_address
+        port = mlConfig.port_number
+        model_directory = mlConfig.model_directory_path
+    except FileNotFoundError:
+        print("Error: Configuration file not found.")
+        return 1
+    #endregion
+
+    processor = AutoProcessor.from_pretrained(model_directory)
+    model = Blip2ForConditionalGeneration.from_pretrained(model_directory, torch_dtype=torch.float16)
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print("Warning: CUDA is not available. Running on CPU.") if not torch.cuda.is_available() else None
+    model.to(device)
+
+    wait_for_messages()
+
+
+if __name__ == "__main__":
+    try:
+        sys.exit(main())
+    except KeyboardInterrupt:
+        print("\nReceiver stopped.")
+        sys.exit(0)
--- mlSubsystem/modelDownloader.py
+++ mlSubsystem/modelDownloader.py
@@ -1,8 +1,25 @@
 from transformers import AutoProcessor, Blip2ForConditionalGeneration
+from mlSubsystem import configreader
 
-# Download and save the processor and model locally
-processor = AutoProcessor.from_pretrained("Salesforce/blip2-opt-2.7b")
-processor.save_pretrained("./local_blip2-opt-2.7b")
 
-model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b")
-model.save_pretrained("./local_blip2-opt-2.7b")
+def main():
+    #region Read Configuration
+    try:
+        mlConfig = configreader.read_config('ml.cfg')
+        model_name = mlConfig.model_name
+        model_dir = mlConfig.model_directory_path
+    except FileNotFoundError:
+        print("Error: Configuration file not found.")
+        return 1
+    #endregio
+
+    # Download and save the processor and model locally
+    processor = AutoProcessor.from_pretrained(model_name)
+    processor.save_pretrained(model_dir)
+
+    model = Blip2ForConditionalGeneration.from_pretrained(model_name)
+    model.save_pretrained(model_dir)
+
+
+if __name__ == "__main__":
+    main()
