2607a3c1246d0b20e2a30cf0e6d90bfa7e645b15
aidandegooyer <aidandegooyer@gmail.com>
2024-10-22 10:35:36 -0400

    upload fix

--- reactFrontend/frontend/src/components/UploadForm.tsx
+++ reactFrontend/frontend/src/components/UploadForm.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Card, Form, Button, Modal, InputGroup } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 import "./UploadForm.css";
 
 const UploadForm: React.FC = () => {
@@ -57,6 +58,7 @@ const UploadForm: React.FC = () => {
                   type="number"
                   placeholder="Enter amount"
                   aria-label="Amount"
+                  onChange={(e) => setPrice(e.target.value)}
                   required
                 />
               </InputGroup>
@@ -81,13 +83,15 @@ const UploadForm: React.FC = () => {
       </Card>
       <Modal show={showModal} onHide={() => setShowModal(false)}>
         <Modal.Header closeButton>
-          <Modal.Title>Success!</Modal.Title>
+          <Modal.Title>Item Successfully Uploaded</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Form submitted successfully!</Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={() => setShowModal(false)}>
             Close
           </Button>
+          <LinkContainer to="/">
+            <Button variant="primary">View Marketplace</Button>
+          </LinkContainer>
         </Modal.Footer>
       </Modal>
     </>
