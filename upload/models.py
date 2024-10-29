adda062d02bfb01297ed1bc168a19298319a8468
Ashley Hamilton <91693425+amhamiltsyr@users.noreply.github.com>
2024-10-29 17:25:50 -0400

    Finished up uploadImage

--- upload/models.py
+++ upload/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class InventoryItem(models.Model):
-	image = models.CharField(max_length=255)
+	image = models.ImageField(upload_to='images/')
 	name = models.CharField(max_length=255)
 	description = models.TextField(default="No Description")
 	price = models.FloatField()
 	itemNumber = models.IntegerField()
 	archieved = models.BooleanField(default=False)
+
