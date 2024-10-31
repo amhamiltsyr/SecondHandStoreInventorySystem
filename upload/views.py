dbd0f4cf73df6de79ee3968e610b04b8f8b70b70
aidandegooyer <aidandegooyer@gmail.com>
2024-10-31 09:49:49 -0400

    fixed views

--- upload/views.py
+++ upload/views.py
@@ -13,52 +13,52 @@ itemNumberOn = 1 # tracks what item number the database will assign the next lis
 # Engages with AI model, returns generated cost and product listing
 def upload_image(request):
 	if request.method == 'POST':
-       		form = ImageUploadForm(request.POST, request.FILES)
-        	if form.is_valid():
-			# Save to database
+		form = ImageUploadForm(request.POST, request.FILES)
+		if form.is_valid():
+		# Save to database
 			form.save()
-			# Call on AI Model
-            		image = form.cleaned_data['image']
-            		cost = sender.send_message(image, "Cost:")
-        		product_listing = sender.send_message(image, "Product Listing:")
-        		# Return suggestions to front end
-			return JsonResponse({
-        			'cost': cost,
-        			'product_listing': product_listing
-       			})
+		# Call on AI Model
+		image = form.cleaned_data['image']
+		cost = sender.send_message(image, "Cost:")
+		product_listing = sender.send_message(image, "Product Listing:")
+		# Return suggestions to front end
+		return JsonResponse({
+					'cost': cost,
+					'product_listing': product_listing
+	   			})
 	return JsonResponse({
-                        'cost': 'error',
-                        'product_listing': 'error'
-        })
+						'cost': 'error',
+						'product_listing': 'error'
+		})
 
 
 # Creates a new item, passed by url parameters
 def create_listing(request, id, name, description, price):
 	item = InventoryItem.objects.get(id = id)
-        item.name = name
-        item.description = description
-        stringPriceToFloat = float(price) # floats cannot be passed via URL
-        item.price = price
-        item.archieved = False
-        item.save()
-        return HttpResponse('Create Listing')
+	item.name = name
+	item.description = description
+	stringPriceToFloat = float(price) # floats cannot be passed via URL
+	item.price = price
+	item.archieved = False
+	item.save()
+	return HttpResponse('Create Listing')
 
 
 # Gets the next 20 items in the inventory database
 def get_next(request,itemsToSend, idToStart, searchTerm=""):
-    all_items = InventoryItem.objects.filter(archieved=False)
+	all_items = InventoryItem.objects.filter(archieved=False)
 
-    if searchTerm:
-        all_items = all_items.filter(
-            Q(name__icontains=searchTerm) | Q(description__icontains=searchTerm)
-        )
-    to_return = all_items[idToStart:idToStart + itemsToSend]
-    to_json = serializers.serialize('json', to_return)
-    total_count = all_items.count()
-    return JsonResponse({
-        'items': to_json,
-        'total_count': total_count
-    })
+	if searchTerm:
+		all_items = all_items.filter(
+			Q(name__icontains=searchTerm) | Q(description__icontains=searchTerm)
+		)
+	to_return = all_items[idToStart:idToStart + itemsToSend]
+	to_json = serializers.serialize('json', to_return)
+	total_count = all_items.count()
+	return JsonResponse({
+		'items': to_json,
+		'total_count': total_count
+	})
 
 
 # Edits item, passed by url parameters
