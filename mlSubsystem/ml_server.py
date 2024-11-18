792681f6013f1ec48ca22453300b5f680b3ea5fa
TownsendSP <tgsoutha@syr.edu>
2024-11-18 00:24:40 -0500

    resizing pt.2

--- mlSubsystem/ml_server.py
+++ mlSubsystem/ml_server.py
@@ -33,6 +33,7 @@ def process_image(image: Image, prompt):
     inputs = processor(image, text=prompt, return_tensors="pt").to(device, torch.float16)
     generated_ids = model.generate(**inputs)
     generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
+    print(f"\tGenerated Text: {generated_text}")
     return generated_text
 
 
